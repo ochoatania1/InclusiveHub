b4702c2653865552651a501ef67e58fbfca32fbc
ochoatania1 <167352125+ochoatania1@users.noreply.github.com>
2024-08-06 05:25:57 -0400

    Add files via upload

---
+++ appbar_title_image.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+
+class AppbarTitleImage extends StatelessWidget {
+  AppbarTitleImage({Key? key, this.imagePath, this.margin, this.onTap})
+      : super(
+          key: key,
+        );
+
+  final String? imagePath;
+
+  final EdgeInsetsGeometry? margin;
+
+  final Function? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onTap?.call();
+      },
+      child: Padding(
+        padding: margin ?? EdgeInsets.zero,
+        child: CustomImageView(
+          imagePath: imagePath!,
+          height: 30.h,
+          width: 26.h,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
---
+++ custom_app_bar.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+
+enum Style {
+  bgGradientnamedeeppurple200nameindigo5001,
+  bgGradientnamedeeppurple200nameindigo50_1,
+  bgGradientnamedeeppurple200nameindigo50
+}
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  CustomAppBar(
+      {Key? key,
+      this.height,
+      this.styleType,
+      this.leadingWidth,
+      this.leading,
+      this.title,
+      this.centerTitle,
+      this.actions})
+      : super(
+          key: key,
+        );
+
+  final double? height;
+
+  final Style? styleType;
+
+  final double? leadingWidth;
+
+  final Widget? leading;
+
+  final Widget? title;
+
+  final bool? centerTitle;
+
+  final List<Widget>? actions;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      toolbarHeight: height ?? 76.h,
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.transparent,
+      flexibleSpace: _getStyle(),
+      leadingWidth: leadingWidth ?? 0,
+      leading: leading,
+      title: title,
+      titleSpacing: 0,
+      centerTitle: centerTitle ?? false,
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size(
+        SizeUtils.width,
+        height ?? 76.h,
+      );
+  _getStyle() {
+    switch (styleType) {
+      case Style.bgGradientnamedeeppurple200nameindigo5001:
+        return Container(
+          height: 76.h,
+          width: double.maxFinite,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0, 0.5),
+              end: Alignment(1, 0.5),
+              colors: [appTheme.deepPurple200, appTheme.indigo5001],
+            ),
+          ),
+        );
+      case Style.bgGradientnamedeeppurple200nameindigo50_1:
+        return Container(
+          height: 86.h,
+          width: double.maxFinite,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0, 0.5),
+              end: Alignment(1, 0.5),
+              colors: [appTheme.deepPurple200, appTheme.indigo50],
+            ),
+          ),
+        );
+      case Style.bgGradientnamedeeppurple200nameindigo50:
+        return Container(
+          height: 76.h,
+          width: double.maxFinite,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0, 0.5),
+              end: Alignment(1, 0.5),
+              colors: [appTheme.deepPurple200, appTheme.indigo50],
+            ),
+          ),
+        );
+      default:
+        return null;
+    }
+  }
+}
